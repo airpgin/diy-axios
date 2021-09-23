4191296c4072ae6199b4d194a812a278bb7c664a
Asher <airpgin@gmail.com>
2021-09-23 09:56:13 +0800

    添加多种请求方法

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
 export default {
-  input: 'lib/axios.js',
+  input: './lib/axios.js',
   output: {
     file: 'dist/bundle.js',
     format: 'iife'
